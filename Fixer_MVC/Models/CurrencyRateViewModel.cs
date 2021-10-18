59bdcbc2f6c1110b2507e781354a876d75854a63
anurag_at_stralfors <anurag.sahajpal@stralfors.no>
2021-10-18 12:29:15 +0200

    added missing files

---
+++ Fixer_MVC/Models/CurrencyRateViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fixer_MVC.Models
+{
+    public class CurrencyRate
+    {
+        [Display(Name = "Symbol")]
+        [Required]
+        public string symbol { get; set; }
+        [Display(Name = "ExchangeRateValue")]
+        [Required]
+        public float value { get; set; }
+
+    }
+
+    public class CurrencyRateViewModel
+    {
+        public CurrencyRateViewModel(List<CurrencyRate> searchResult = null)
+        {
+
+            CurrencyRates = searchResult ?? new List<CurrencyRate>();
+        }
+
+        ///<summary>
+        /// Gets or sets Customers.
+        ///</summary>
+        public List<CurrencyRate> CurrencyRates { get; set; }
+
+    }
+
+}
