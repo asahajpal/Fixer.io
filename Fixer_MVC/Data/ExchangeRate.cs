a6e65013cc7e7e94a7de5c6027045249f8a8b6fa
anurag_at_stralfors <anurag.sahajpal@stralfors.no>
2021-10-22 00:35:00 +0200

    added DbContext class - ExchangeRateContext

---
+++ Fixer_MVC/Data/ExchangeRate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fixer_MVC.DataModel
+{
+    public class ExchangeRate
+    {
+        public int ID { get; set; }
+     
+        public string SymbolBaseCurr { get; set; }
+
+        public string SymbolTargetCurr { get; set; }
+
+        public float ExchangeRateValue { get; set; }
+
+        public DateTime ExchangeRatetDateTime { get; set; }
+    }
+}
---
+++ Fixer_MVC/Data/ExchangeRateContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fixer_MVC.DataModel
+{
+    public class ExchangeRateContext : DbContext
+    {
+        public ExchangeRateContext(DbContextOptions<ExchangeRateContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ExchangeRate> ExchangeRates { get; set; }
+
+        // default behaviour of creating data table with plural name is overriden 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ExchangeRate>().ToTable("ExchangeRate");
+        }
+    }
+
+}
